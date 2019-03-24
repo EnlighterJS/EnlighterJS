48888a1391e792b5f515c3d650b6797d9c6b317a
Pascl Havé <code@haveneer.com>
2019-03-24 19:52:50 +0100

    Add Scala support

---
+++ src/lang/scala.js
@@ -0,0 +1,158 @@
+// ----------------------------------------------------------------------
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// --
+// Copyright 2016-2019 Andi Dittrich <https://andidittrich.de>
+// ----------------------------------------------------------------------
+
+// Generic Rules/Regex
+import _language_common_rules from '../engine/generic-rules';
+import {generic} from './generic';
+
+// Scala Language
+// Author: [Pascal Havé]
+// keywords from https://www.oreilly.com/library/view/learning-scala/9781449368814/apa.html
+// --
+export class scala extends generic {
+
+    setupLanguage() {
+
+        this.rules = [
+
+            // strings, chars
+            _language_common_rules.dqStrings,
+            _language_common_rules.char,
+
+            // string interpolation : s"hello ${world}"
+            {
+                regex: /s"(?:[^"\\]|\\.)*"/g,
+                type: 's5'
+            },
+
+            // backticks identifier
+            {
+                regex: /`(?:[^`\\]|\\.)*`/g,
+                type: 'e4'
+            },
+
+            // annotations
+            {
+                regex: /@[\W\w_][\w]+/g,
+                type: 'k7'
+            },
+
+             // basic types
+            {
+                regex: /\b(Boolean|Byte|Int|Long|Float|Double|Char|String|Unit)\b/g,
+                type: 'k5'
+            },
+
+            // template types
+            {
+                regex: /\[\s*([-+]?[A-Z]\w*)\s*\]/g,
+                type: 'k5'
+            },
+
+            // types
+            {
+                regex: /\b([A-Z]\w*)\b/g,
+                type: 'k5'
+            },
+
+            // control keywords
+            {
+                regex: /\b(while|try|catch|else|throw|break|if|do|goto|switch|for|match|yield)\b/g,
+                type: 'k0'
+            },
+
+            // package directives
+            {
+                regex: /(package|import)(\s+[\w.]+)/gm,
+                type: ['k0', 'text']
+            },
+
+            // symbol keywords
+            {
+                regex: /[\b\w\s)](_|:|@|#|<-|←|<:|<%|=|=>|⇒|>:)[\b\w\s]/g,
+                type: 'k3'
+            },
+
+            // keywords
+            {
+                regex: /\b(abstract|case|class|extends|final|finally|forSome|implicit|lazy|object|override|private|protected|return|sealed|trait|with)\b/g,
+                type: 'k0'
+            },
+
+            // def keyword
+            {
+                regex: /\b(def)\s+(\w+)\b/gm,
+                type: ['k0', 't5']
+            },
+
+            // type keyword
+            {
+                regex: /\b(type)\s+(\w+)\b/gm,
+                type: ['k0', 'k4']
+            },
+
+            // val keyword
+            {
+                regex: /\b(val)\s+(\w+)\b/gm,
+                type: ['k0', 'text']
+            },
+
+            // var keyword
+            {
+                regex: /\b(var)\s+(\w+)\b/g,
+                type: ['t6', 'text']
+            },
+
+            // special inheritance
+            {
+                regex: /\b(this|super)\b/g,
+                type: 'k2'
+            },
+
+            // operator
+            {
+                regex: /\b(new)\b/g,
+                type: 'k3'
+            },
+
+            // function/method calls
+            {
+                regex: /\b([a-z][\w]*)\s*(?:\[(.*?)\])?\s*\(/gm,
+                type: ['t5', 't6']
+            },
+            {
+                regex: /\.\s*([a-z][\w]*)/gm,
+                type: 't5'
+            },
+            {
+                regex: /\b[a-z]\w*\s+([a-z][\w]*)\b/gm,
+                type: 't5'
+            },
+
+            // boolean values, null keywords
+            _language_common_rules.null,
+            _language_common_rules.boolean,
+
+            // comments
+            _language_common_rules.slashComments,
+            _language_common_rules.blockComments,
+            _language_common_rules.docComments,
+
+            // numbers
+            _language_common_rules.int,
+            _language_common_rules.floats,
+            _language_common_rules.bin,
+            _language_common_rules.hex,
+            _language_common_rules.octal,
+
+            // brackets
+            _language_common_rules.brackets
+
+        ];
+    }
+}
