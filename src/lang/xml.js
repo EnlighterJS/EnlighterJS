77dc8d637352132ed2529ff0c63557419168b2a7
Andi Dittrich <andi@andidittrich.de>
2020-05-21 17:21:41 +0200

    fixed #108

--- src/lang/xml.js
+++ src/lang/xml.js
@@ -27,7 +27,7 @@ export class xml extends generic {
         // xml attributes. Stage-2 Analyzing
         function parseXmlAttributes(token){
             // run the MicroTokenizer to identify the name=value match
-            return _microTokenizer(token, /\b([\w:-]+)(\s*=\s*)(['"][^'"]*['"]|[^'" \t]+)/gi, function(match){
+            return _microTokenizer(token, /\b([\w:-]+)(\s*=\s*)((['"]).*?\4|[^'" \t]+)/gi, function(match){
                 // attribute name | assignment operator | attribute value (string)
                 return [_token(match[1], 'x2'), _token(match[2], 'k3'), _token(match[3], 's0')];
             });
