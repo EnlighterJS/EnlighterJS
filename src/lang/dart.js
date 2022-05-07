51017c58638ffbe901877be9a1d0037d1cee281d
Andi Dittrich <andi@andidittrich.de>
2022-05-07 13:30:50 +0200

    bumped deps; merged changelog; fixed version to 3.5

--- src/lang/dart.js
+++ src/lang/dart.js
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // --
-// Copyright 2016-2018 Andi Dittrich <https://andidittrich.de>
+// Copyright 2016-2022 Andi Dittrich <https://andidittrich.com>, 2022 Minas Giannekas
 // ----------------------------------------------------------------------
 
 // Generic Rules/Regex
